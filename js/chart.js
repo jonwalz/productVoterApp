8e11724971b2dfa1969fd19deb03a059015ac834
Jon Walz <jon.walz.music@gmail.com>
2016-07-30 11:01:17 -0700

    Custom colors

--- js/chart.js
+++ js/chart.js
@@ -1,31 +1,46 @@
+var chart;
 function renderChart() {
-    	var chart = new CanvasJS.Chart("chartContainer", {
+    	chart = new CanvasJS.Chart("chartContainer", {
+        backgroundColor: "#588C7E",
+        axisX : {
+            labelFontColor: "#F2E394"
+        },
+        axisY : {
+            labelFontColor: "#F2E394"
+        },
 	    title: {
-	        text: "Voting Data"
+	        text: "Voting Data",
+            fontColor: "#F2E394"
 	    },
 	    data: [{
 	        // Change type to "doughnut", "line", "splineArea", etc.
 	        type: "column",
 	        dataPoints: [{
+                color: '#FF974F',
 	            label: imageOptions[0].name,
 	            y: imageOptions[0].upVotes
 	        }, {
+                color: '#FF974F',
 	            label: imageOptions[1].name,
 	            y: imageOptions[1].upVotes
 	        },
             {
+                color: '#FF974F',
 	            label: imageOptions[2].name,
 	            y: imageOptions[2].upVotes
 	        },
             {
+                color: '#FF974F',
 	            label: imageOptions[3].name,
 	            y: imageOptions[3].upVotes
 	        },
             {
+                color: '#FF974F',
 	            label: imageOptions[4].name,
 	            y: imageOptions[4].upVotes
 	        },
             {
+                color: '#FF974F',
 	            label: imageOptions[5].name,
 	            y: imageOptions[5].upVotes
 	        }]
