f449d080563413cc7ffc41fb6093c0261799e86f
Jon Walz <jon.walz.music@gmail.com>
2016-08-03 13:40:05 -0700

    Added some logic, still refactoring

--- js/chart.js
+++ js/chart.js
@@ -1,53 +1,96 @@
 var chart;
-function renderChart() {
-    	chart = new CanvasJS.Chart("chartContainer", {
-		height: 260,		
-        backgroundColor: "#4EAB59",
-        axisX : {
-            labelFontColor: "#F2E394"
-        },
-        axisY : {
-            labelFontColor: "#F2E394"
-        },
-	    title: {
-	        text: "Voting Data",
-            fontColor: "#F2E394"
-	    },
-	    data: [{
-	        // Change type to "doughnut", "line", "splineArea", etc.
-            indexLabelFontColor: "#F2E394",
-	        type: "bar",
-	        dataPoints: [{
-                color: '#FF974F',
-	            label: imageOptions[0].name,
-	            y: imageOptions[0].upVotes
-	        }, {
-                color: '#d47e42',
-	            label: imageOptions[1].name,
-	            y: imageOptions[1].upVotes
-	        },
-            {
-                color: '#b16835',
-	            label: imageOptions[2].name,
-	            y: imageOptions[2].upVotes
-	        },
-            {
-                color: '#8a5029',
-	            label: imageOptions[3].name,
-	            y: imageOptions[3].upVotes
-	        },
-            {
-                color: '#704120',
-	            label: imageOptions[4].name,
-	            y: imageOptions[4].upVotes
-	        },
-            {
-                color: '#4e2d16',
-	            label: imageOptions[5].name,
-	            y: imageOptions[5].upVotes
-	        }]
-	    }]
+
+// datapoints constructor
+var dataPointsVotes = [];
+var dataPointsViews = [];
+
+function dataConstruct() {
+	imageOptions.forEach(function(el, i) {
+		dataPointsVotes.push({
+			y: imageOptions[i].upVotes,
+			label: imageOptions[i].name
+		});
 	});
-    chart.render();
+
+	imageOptions.forEach(function(el, i) {
+		dataPointsViews.push({
+			y: imageOptions[i].displays
+		});
+	});
+
+	console.log(dataPointsViews);
 }
-	document.getElementById('container').addEventListener("click", recordClick, true);
+console.log("data views:" + dataPointsViews);
+
+function renderChart() {
+
+	// set width of chart 
+	var chartWidthRef = document.getElementById('chartStyle').clientWidth;
+
+	chart = new CanvasJS.Chart("chartContainer", {
+		width: chartWidthRef,
+		height: 260,
+		backgroundColor: "#4EAB59",
+		axisX: {
+			labelFontColor: "#F2E394",
+			title: "Items",
+			titleFontColor: '#F2E394'
+		},
+		axisY: {
+			labelFontColor: "#F2E394",
+			title: "Number of Votes",
+			titleFontColor: '#F2E394'
+		},
+		title: {
+			text: "Voting Data",
+			fontColor: "#F2E394",
+		},
+		data: [{
+			indexLabelFontColor: "#F2E394",
+			type: "stackedBar100",
+			dataPoints: dataPointsVotes,
+			// dataPoints: [{
+			// 	color: '#FF974F',
+			// 	label: imageOptions[0].name,
+			// 	y: imageOptions[0].upVotes
+			// }, {
+			// 	color: '#d47e42',
+			// 	label: imageOptions[1].name,
+			// 	y: imageOptions[1].upVotes
+			// }, {
+			// 	color: '#b16835',
+			// 	label: imageOptions[2].name,
+			// 	y: imageOptions[2].upVotes
+			// }, {
+			// 	color: '#8a5029',
+			// 	label: imageOptions[3].name,
+			// 	y: imageOptions[3].upVotes
+			// }, {
+			// 	color: '#704120',
+			// 	label: imageOptions[4].name,
+			// 	y: imageOptions[4].upVotes
+			// }, {
+			// 	color: '#4e2d16',
+			// 	label: imageOptions[5].name,
+			// 	y: imageOptions[5].upVotes
+			// }]
+		}, {
+			type: "stackedBar100",
+			dataPoints: [{
+				y: imageOptions[0].displays - imageOptions[0].upVotes
+			}, {
+				y: imageOptions[1].displays - imageOptions[1].upVotes
+			}, {
+				y: imageOptions[2].displays - imageOptions[2].upVotes
+			}, {
+				y: imageOptions[3].displays - imageOptions[3].upVotes
+			}, {
+				y: imageOptions[4].displays - imageOptions[4].upVotes
+			}, {
+				y: imageOptions[5].displays - imageOptions[5].upVotes
+			}, ]
+		}]
+	});
+	chart.render();
+}
+document.getElementById('container').addEventListener("click", recordClick, true);
--- js/js.js
+++ js/js.js
@@ -3,27 +3,26 @@ var imageTracker = function(name, source) {
     this.imageSource = source;
     this.upVotes = 0;
     this.name = name;
+    this.displays = 0;
 };
 
 
 var imageOptions = [
-  new imageTracker("Banana", "images/banana.jpg"),
-  new imageTracker("Bag", "images/bag.jpg"),
-  new imageTracker("Boots", "images/boots.jpg"),
-  new imageTracker("Chair", "images/chair.jpg"),
-  new imageTracker("Cthulhu", "images/cthulhu.jpg"),
-  new imageTracker("Dragon", "images/dragon.jpg"),
-  new imageTracker("Pen", "images/pen.jpg"),
-  new imageTracker("Scissor", "images/scissors.jpg"),
-  new imageTracker("Shark", "images/shark.jpg"),
-  new imageTracker("Sweep", "images/sweep.jpg"),
-  new imageTracker("Unicorn", "images/unicorn.jpg"),
-  new imageTracker("Usb", "images/usb.jpg"),
-  new imageTracker("Water Can", "images/water_can.jpg"),
-  new imageTracker("Wine Glass", "images/wine_glass.jpg"),
+    new imageTracker("Banana", "images/banana.jpg"),
+    new imageTracker("Bag", "images/bag.jpg"),
+    new imageTracker("Boots", "images/boots.jpg"),
+    new imageTracker("Chair", "images/chair.jpg"),
+    new imageTracker("Cthulhu", "images/cthulhu.jpg"),
+    new imageTracker("Dragon", "images/dragon.jpg"),
+    new imageTracker("Pen", "images/pen.jpg"),
+    new imageTracker("Scissor", "images/scissors.jpg"),
+    new imageTracker("Shark", "images/shark.jpg"),
+    new imageTracker("Sweep", "images/sweep.jpg"),
+    new imageTracker("Unicorn", "images/unicorn.jpg"),
+    new imageTracker("Usb", "images/usb.jpg"),
+    new imageTracker("Water Can", "images/water_can.jpg"),
+    new imageTracker("Wine Glass", "images/wine_glass.jpg"),
 ];
-
-// document.getElementById('container').addEventListener("click", recordClick);
 
 function getThreeImages() {
     var pickedImages = [];
@@ -35,6 +34,10 @@ function getThreeImages() {
         document.getElementById("img" + imageId).src = source;
         pickedImages.push(index);
     }
+    // track displayed images
+        pickedImages.forEach(function(el){
+            imageOptions[el].displays++;
+        });
 }
 
 function recordClick(event) {
@@ -44,15 +47,14 @@ function recordClick(event) {
             imageOptions[index].upVotes++;
         }
     }
+
     getThreeImages();
     checkVotes();
-
-    console.log(imageOptions[0].upVotes);
 }
 
 (function removeVotesEl() {
     var el = document.getElementById('votes');
-    el.parentNode.style.display = "none"; 
+    el.parentNode.style.display = "none";
 })();
 
 function checkVotes() {
@@ -61,29 +63,43 @@ function checkVotes() {
         var el = document.createElement('div');
         var elH1 = document.createElement('h1');
         el.className = "totalVotes";
-        el.innerText = "You have reached 15 votes. This is what you voted for:" ;
+        el.innerText = "You have reached 15 votes.";
+        var br = document.createElement('br');
+
+        var h12 = document.createElement('h1');
+        h12.innerText = "This is what you voted for:";
         var parentEl = document.getElementById('votes');
 
         parentEl.appendChild(elH1).appendChild(el);
-
+        parentEl.appendChild(h12);
         var removeText = document.getElementById('instructions');
         removeText.parentNode.remove();
 
         var votesList = document.createElement('ul');
         votesList.className = "votesList";
         for (var i = 0; i < imageOptions.length; i++) {
-            if(imageOptions[i].upVotes != 0) {
+            if (imageOptions[i].upVotes !== 0) {
                 var voteLi = document.createElement('li');
                 voteLi.innerText = imageOptions[i].name + ": " + imageOptions[i].upVotes;
                 votesList.appendChild(voteLi);
             }
         }
-
+        
+        dataConstruct();
         parentEl.appendChild(votesList);
-        // document.getElementById('container').removeEventListener("click", recordClick);
         document.getElementById('votes').parentNode.style.display = "Block";
+
+        var chartHide = document.getElementById('hideChart');
+        chartHide.style.display = 'block';
+        
         renderChart();
     }
 }
-
 getThreeImages();
+
+// hide the chart container before rendering
+
+(function hideChart() {
+    var chartHide = document.getElementById('hideChart');
+    chartHide.style.display = 'none';
+})();
