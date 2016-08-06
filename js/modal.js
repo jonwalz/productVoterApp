1a56d556876d4b44b9f42261a231aaf7b0f8f34a
Jon Walz <jon.walz.music@gmail.com>
2016-08-05 21:00:15 -0700

    Made modal appear if username not detected in localstorage

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,17 @@
+// show modal if local storage: userName is empty
+
+if (!localStorage.getItem('userName')) {
+    var modalContainer = document.getElementById('modalContainer');
+    modalContainer.style.display = 'flex';
+} else {
+    var modalContainer = document.getElementById('modalContainer');
+    modalContainer.style.display = 'none';
+}
+
 // assign the users input to a variable after button press
 var goButton = document.getElementById('goButton');
 var userNameGlobal = '';
+
 
 function goName() {
     var userName = document.getElementById('userName').value;
@@ -18,3 +29,10 @@ function goName() {
 goButton.addEventListener('click', goName, false);
 
 // close modal on x press
+
+function closeModal(){
+    var modalContainer = document.getElementById('modalContainer');
+    modalContainer.style.display = 'none';
+}
+
+document.getElementById('modalExit').addEventListener('click', closeModal);
